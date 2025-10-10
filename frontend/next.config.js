966f62a2ba18ab85d33765cf10d4eafc1dd016ad
Kevin-Shelton <167843194+ksegis@users.noreply.github.com>
2025-10-10 11:33:06 -0600

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,11 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
-
