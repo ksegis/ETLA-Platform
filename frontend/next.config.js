2dfd5befb7516b0a8fcb09214fb428013aab61c3
Kevin-Shelton <167843194+ksegis@users.noreply.github.com>
2025-10-17 22:37:55 -0600

    Set TS baseUrl=src and add webpack aliases for components/lib/contexts

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,11 @@
-const path = require('path');
+﻿const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+  webpack: (config) => { config.resolve.alias["components"] = path.resolve(__dirname, "src/components"); config.resolve.alias["lib"] = path.resolve(__dirname, "src/lib"); config.resolve.alias["contexts"] = path.resolve(__dirname, "src/contexts"); config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
   },
 };
 
 module.exports = nextConfig;
+
