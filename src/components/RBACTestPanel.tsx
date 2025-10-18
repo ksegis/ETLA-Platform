3fe82c2bee457ddd2b9dbd7b557214840ad1ef1d
Kevin-Shelton <167843194+ksegis@users.noreply.github.com>
2025-10-18 14:35:41 -0600

    Restore corrupted requests page.tsx file

---
+++ src/components/RBACTestPanel.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+export default function RBACTestPanel() {
+  return (
+    <div>
+      <h1>RBAC Test Panel - Simple Version</h1>
+    </div>
+  );
+}
