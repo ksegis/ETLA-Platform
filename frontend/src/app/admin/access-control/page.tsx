f37b137b5b9d915fbf160ff3d2d98774a0d4dd01
Kevin-Shelton <167843194+ksegis@users.noreply.github.com>
2025-10-20 23:40:38 -0600

    Fix: Remove server-side exports from all client components

--- frontend/src/app/admin/access-control/page.tsx
+++ frontend/src/app/admin/access-control/page.tsx
@@ -1,13 +1,11 @@
-﻿// DO NOT add 'use client' here
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-export const fetchCache = 'force-no-store';
+// DO NOT add 'use client' here
 
 import AccessControlClient from './AccessControlClient';
 
 export default function Page() {
   return <AccessControlClient />;
 }
+
 
 
 
--- frontend/src/app/reporting/tax-records/page.tsx
+++ frontend/src/app/reporting/tax-records/page.tsx
@@ -1,8 +1,5 @@
 'use client'
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-export const fetchCache = 'force-no-store';
 
 import React from 'react'
 import DashboardLayout from '../../../components/layout/DashboardLayout'
@@ -23,6 +20,7 @@ export default function TaxRecordsPage() {
     </DashboardLayout>
   )
 }
+
 
 
 
